92ef3bf1cb58953dde91b06d86dbef2aa6bfcd19
spacedestroyerznkggz <86682248+Danish150@users.noreply.github.com>
2024-06-15 11:15:34 +0700

    Make roundHandler promise loop Fully Functional

--- src/server/roundHandler.server.ts
+++ src/server/roundHandler.server.ts
@@ -1,71 +1,46 @@
 import { Settings } from "shared/settings";
 import * as Status from "shared/status";
-
 import { Players } from "@rbxts/services";
 import * as Events from "server/events";
 
-const GetNumPlayers = () => Players.GetPlayers().size();
+const getNumPlayers = () => Players.GetChildren().size();
+const enoughPlayers = () => getNumPlayers() >= 2;
+const updateRoundState = (state: string) => { /*SharedRoundConfig.SetAttribute("State", state);*/ };
 
-const enoughPlayers = () => GetNumPlayers() >= 2
+const enoughPlayersPromise = (): Promise<unknown> => 
+    enoughPlayers() ? Promise.resolve(true) : Promise.fromEvent(Players.ChildAdded, () => enoughPlayers());
 
-const updateRoundState = (state: string) => {
-	//SharedRoundConfig.SetAttribute("State", state);
-}
+const notEnoughPlayersPromise = (): Promise<unknown> => 
+    !enoughPlayers() ? Promise.resolve(true) : Promise.fromEvent(Players.ChildRemoved, () => !enoughPlayers());
 
-const getRandomGameMode = () => {
-    //todo
-}
+const gameLoop = () => {
+    print('Running');
+    updateRoundState("InGame");
+    task.wait(5);
+    waitForPlayers();
+};
 
-// Promises
-const enoughPlayersPromise = (): Promise<unknown> => {
-	if (enoughPlayers()) {
-		return Promise.resolve(true);
-	}
-    
-	return Promise.fromEvent(Players.ChildAdded, () => {return enoughPlayers()} )
-}
+const intermission = () => {
+    print("Intermission");
+    updateRoundState("Intermission");
 
-const notEnoughPlayersPromise = (): Promise<unknown> => {
-    if (!enoughPlayers()) {
-		return Promise.resolve(true);
-	}
+    Promise.race([
+        notEnoughPlayersPromise().then(() => Promise.resolve("Return")),
+        Promise.delay(5).then(() => Promise.resolve("Start")),
+    ])
+    .then((message) => {
+        if (message === "Start") {
+            gameLoop();
+        } else {
+            waitForPlayers();
+        }
+    });
+};
 
-	return Promise.fromEvent(Players.ChildRemoved, () => {return !enoughPlayers()} )
-}
+const waitForPlayers = async () => {
+    print("Waiting for players");
+    updateRoundState("Waiting For Players");
+    enoughPlayersPromise().then(intermission);
+};
 
-// Game State Functions
-const intermit = (): unknown =>  {
-	print("Intermission");
-	updateRoundState("Intermission");
-	return Promise.race([
-		notEnoughPlayersPromise().then( () => {
-			return waitForPlayers()
-		} ),
-
-		Promise.delay(5)
-			.then( () => {
-                print('Running')
-                task.wait(5)
-            })
-			.then(waitForPlayers)
-			.catch(() => {
-				warn("Failed to start game mode", error);
-				return waitForPlayers();
-			}),
-	]);
-}
-
-const waitForPlayers = () => {
-	print("Waiting for players");
-	updateRoundState("Waiting For Players");
-	return enoughPlayersPromise().then( () => {
-		intermit()
-	} );
-}
-
-// Main
-
-const nextBlock = waitForPlayers;
-
-// eslint-disable-next-line no-constant-condition
-nextBlock().expect();
+waitForPlayers().expect();
